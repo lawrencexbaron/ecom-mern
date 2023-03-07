e773ebe8b1c1455647365c12b1547fad3d441574
Lawrence <lawrencebaron027@gmail.com>
2023-03-07 09:17:17 -0700

    Added limit on image upload

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ const productRoutes = require("./routes/productRoutes");
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
