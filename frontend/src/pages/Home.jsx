29ab1693dcffb9a2051c4360148d503ee8168006
Lawrence <lawrencebaron027@gmail.com>
2023-03-08 14:38:06 -0700

    Added sidebar filter for items

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,17 +19,25 @@ const Home = () => {
     <>
       <main className="px-8 py-6 h-full">
         <h1 className="text-4xl font-bold mb-4">Products</h1>
-        <div className="grid sm:grid-cols-4 gap-2">
-          <Card
-            props={{
-              products,
-              addToCart,
-              removeFromCart,
-              clearCart,
-              deleteProduct,
-              fetchProducts,
-            }}
-          />
+        <div className="flex">
+          <div className="hidden sm:flex sm:flex-col sm:w-1/4">
+            <p className="font-bold text-xl mt-5">Results</p>
+            <p className="font-semibold text-2xl mt-3">
+              {products.length} Items
+            </p>
+          </div>
+          <div className="grid sm:grid-cols-4 gap-2">
+            <Card
+              props={{
+                products,
+                addToCart,
+                removeFromCart,
+                clearCart,
+                deleteProduct,
+                fetchProducts,
+              }}
+            />
+          </div>
         </div>
       </main>
     </>
